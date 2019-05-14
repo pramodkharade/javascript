6e759d6906fa11b73db54f9fe98bc37168f0f0b3
pramod.kharade <pramod.kharade@blazeclan.com>
2019-05-14 12:44:54 +0530

    forEach looping

--- app.js
+++ app.js
@@ -16,3 +16,9 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 for (let i = companies.length - 1; i >= 0; i--) {
 	console.log(companies[i]);
 }
+
+/***forEach loop**/
+console.log('forEach loop');
+companies.forEach(function(company){
+	console.log(company);
+});
