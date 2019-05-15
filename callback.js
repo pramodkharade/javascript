9783d7c695d4c367c10144c4b33c78c27a00f775
pramod.kharade <pramod.kharade@blazeclan.com>
2019-05-15 10:02:21 +0530

    callback in javascript

---
+++ callback.js
@@ -0,0 +1,29 @@
+const posts = [{
+	'title':'Post one',
+	'body':'Post Body one'
+},
+{
+	'title':'Post Two',
+	'body':'Post Body two'
+}];
+
+
+function getPosts(){
+	setTimeout(()=>{
+		let output='';
+		posts.forEach((post,index)=>{
+			output += `<li>${post.title}</li>`;
+		});
+		document.getElementById('postwrapper').innerHTML = output;
+	},1000);
+}
+
+
+function createPost(post,callback){
+	setTimeout(()=>{
+		posts.push(post);
+		callback();
+	},2000);
+}
+
+createPost({'title':'post three','body':'Post body three'},getPosts);
